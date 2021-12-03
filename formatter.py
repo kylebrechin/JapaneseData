9f9396abaa2ce28bd5eec644ea3b4f9bce4be030
kylebrechin <kyle.brechin@gmail.com>
2021-12-02 20:44:26 -0800

    Update formatter.py
    Kana complete
    - check for kanji/kana and insert kana twice if no kanji
    - insert sentence count

--- formatter.py
+++ formatter.py
@@ -48,7 +48,16 @@
 # print(" -- finished removing brackets --")
 
 # ####-------------------------------------------------####
-# #                  Fix Double Kana - START
+# #                  Fix Only Kana - START
+# ####-------------------------------------------------####
+#   This section actually has to take care of a few things.
+#
+#   We need to find if there is only a kana for the sentence,
+#   from there we input the kana we found onto the next line.
+#   Then we check to see if there is only one sentence for the word
+#   by getting the grammar for the word and comparing it to
+#   where the next grammar type should be based on index
+#   if the next word has a kana or not
 # ####-------------------------------------------------####
 
 ##############
@@ -56,102 +65,68 @@
 ##############
 TYPES_OF_GRAMMAR = ["Adjective", "Adjectival Noun", "Adverb", "Noun", "Interrogative", "Verbal Noun",
                      "Verb", "Interjection", "Phrase", "Pronoun"]
-# used to set current_line_being_read if one sentence
-ONE_S_SPACING = 8               # 7
-# used to set current_line_being_read if two sentences
-TWO_S_SPACING = 11              # 10
-# used in checking from GRAMMAR line if next has single sentences, with or without a corrected kana
-ONE_S_NO_KANA = 7       # 6
-ONE_S_WITH_KANA = 8     # 7
-# used in checking from GRAMMAR line if next has double sentences, with or without a corrected kana
-TWO_S_NO_KANA = 10      # 9
-TWO_S_WITH_KANA = 11    # 10
-# ^ these two no longer used
+# the current line being read
+current_line = 0
+# set to where the grammar should be if there is a kana in the words data set
+grammar_line = 0
+not_finished = True
 
-# +1 to find line in Sublime from here
-# -1 to find line from Sublime from here
-current_line = 0                            # this like should have the kanji ( DEFAULT - 0)
-kana_insert_idx = current_line + 1          # where to insert the kana
-kana_line = current_line + 1                # this like should have the kana
-grammar_line = current_line + 3             #  this line should have the grammar
-sentence_insert_idx = grammar_line + 1
+def isGrammarType(to_check):
+    return to_check in TYPES_OF_GRAMMAR
 
-# debug lines
-grammar_check_line_1 = grammar_line + TWO_S_NO_KANA
-grammar_check_line_2 = grammar_line + TWO_S_WITH_KANA
+with open('data.txt', 'r', encoding="utf8") as in_file:
 
-def DebugLines():
-    print("{0:>27}{1:>10}".format("Line", "Content"))
-    print("{0:<23}{1:<7}{2}".format("Cur. Line - Kanji", (current_line + 1), (filedata[current_line].strip())))
-    print("{0:<23}{1:<7}{2}".format("Kana", (kana_line + 1), (filedata[kana_line].strip())))
-    print("{0:<23}{1:<7}{2}".format("Grammar", (grammar_line + 1), (filedata[grammar_line].strip())))
-    print("{0:<23}{1:<7}".format("Sentence Insert", (sentence_insert_idx + 1)))
-    print("{0:<23}{1:<7}{2}".format("Grammar Checks", (grammar_check_line_1), (grammar_check_line_2)))
-    for idx, item in enumerate(filedata[current_line:current_line + 20]):
-        print(f"{idx + 1}:\t{item}", end='')
+    filedata = in_file.readlines()                                      # read in a list of all the data in the file
+
+    # while NOT last_line or current_line > len(filedata)
+    while not_finished or current_line < len(filedata):
+        current_line_data = filedata[current_line].strip()                  # data stored in the current line, stripped to remove \n
+        grammar_line = current_line + 3
+        grammar_data = filedata[grammar_line].strip()                       # data stored in the grammar line, stripped to remove \n
 
 
-with open('sentences_different.txt', 'r', encoding="utf8") as in_file:
-    filedata = in_file.readlines()
-    last = False
-    while current_line < len(filedata):
+        # Check to see if word set has a kana
+        # This is done by checking to see if the grammar type
+        # matches any of the available types
+            # True = kana is on 2nd line
+            # False = there is currently no kana
+        if not isGrammarType(grammar_data):                                 # no kana found: perform kana insertion operation
+            filedata.insert(current_line + 1, current_line_data+'\n')       # insert the 2nd kana into the list
 
-        # *** DEBUG INFO ***
+        # We're going to assume this word set only has one sentence
+        # so we're going to look for:
+            # check_1: grammar if no kana
+            # check_2: grammar if has kana
         try:
-            DebugLines()
-        except:
-            sentence_insert_idx = grammar_line + 1
+            check_1 = filedata[current_line + 9].strip()
+            check_2 = filedata[current_line + 10].strip()
+        except IndexError:
+            not_finished = False
 
-        # Check if a kana comes after a kanji, if not: there is no kana, so insert one
-        #try:
-        if filedata[grammar_line].strip()  in TYPES_OF_GRAMMAR:
-            print("kana is correct")
-            # write the kana into the file
-        else:
-            print("\nkana is NOT correct")
-            print("-- inserting kana --\n")
-            filedata.insert(kana_insert_idx, filedata[current_line])
-            DebugLines()
-        input("")
-        #except IndexError:
-            #print("IndexError::No more lines to read")
-
-
-        ####-------------------------------------------------####
-        #        Check if one or two sentence
-        ####-------------------------------------------------####
-        try:    # TODO: search single sentence instead, essential swap the if/else
-            if filedata[(grammar_line + ONE_S_NO_KANA)].strip() not in TYPES_OF_GRAMMAR and \
-                    filedata[(grammar_line + ONE_S_WITH_KANA)].strip() not in TYPES_OF_GRAMMAR:
-                #print("Should be two sentence -- inserting count")
-                filedata.insert(sentence_insert_idx, "2\n")
-                current_line += TWO_S_SPACING
+        try:    # get sentence count
+            if isGrammarType(check_1):
+                sentence_count = 1
+                filedata.insert(grammar_line + 1, str(sentence_count) +'\n')
+            elif isGrammarType(check_2):
+                sentence_count = 1
+                filedata.insert(grammar_line + 1, str(sentence_count) +'\n')
             else:
-                #print("Should be one sentence -- inserting count")
-                filedata.insert(sentence_insert_idx, "1\n")
-                current_line += ONE_S_SPACING
-        except IndexError: # do the last!
-            print("IndexError::EOF - Should be one sentence")
-            last = True
-            print(f'Count insert: {sentence_insert_idx + 1}')
-            filedata.insert(sentence_insert_idx, "1\n")
-            current_line += ONE_S_SPACING
+                sentence_count = 2
+                filedata.insert(grammar_line + 1, str(sentence_count) +'\n')
+        except IndexError:
+            sentence_count = 1
+            filedata.insert(grammar_line + 1, str(sentence_count) +'\n')
+            print("GrammarCheck::IndexError:")
 
-        ####-------------------------------------------------####
-        #                   Update Counters
-        ####-------------------------------------------------####
-        kana_insert_idx = current_line + 1
-        kana_line = current_line + 1  # this like should have the kana
-        grammar_line = current_line + 3  # this line should have the grammar
-        sentence_insert_idx = grammar_line + 1
-        # debug lines
-        grammar_check_line_1 = grammar_line + TWO_S_NO_KANA
-        grammar_check_line_2 = grammar_line + TWO_S_WITH_KANA
-
-    print(" -- finished fixing kana --")
+        if sentence_count == 1:
+            current_line += 8
+        elif sentence_count == 2:
+            current_line += 11
+        else:
+            print("InvalidValue::sentence_count")
 
 # # ---------- SAVE FILE ----------
-with open('sentences_numbered.txt', 'w', encoding="utf8") as out_file:
+with open('data_out.txt', 'w', encoding="utf8") as out_file:
     for line in filedata:
         out_file.write(line)
 
